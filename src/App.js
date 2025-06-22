408271051a2f5b8a91e97ccdb0544fc5be6f530d
vdelaconcep <vdelaconcecep@gmail.com>
2025-06-22 01:12:16 -0300

    Falta formato del mensaje de bienvenida

--- src/App.js
+++ src/App.js
@@ -15,12 +15,19 @@ const App = () => {
     setMensaje(true);
   }
 
+  const limpiarFormulario = () => {
+    setPersona({
+      nombre: "",
+      edad: ""
+    });
+  };
+
   return (
     <>
       <div className='formulario-div bg-info text-white'>
         <h2 className='cursiva formulario-titulo fw-bold text-center p-2'>Práctico n°11 - Nombre y edad</h2>
         <form onSubmit={gestionEnvio}>
-          <input className='montserrat formulario-input form-control text-center mt-3'
+          <input className='montserrat formulario-input text-center text-white mt-3'
             type='text'
             value={persona.nombre}
             onChange={(event) => setPersona((anterior) => ({...anterior, nombre: event.target.value}))}
@@ -29,7 +36,7 @@ const App = () => {
             required
           />
           
-          <input className='montserrat formulario-input form-control text-center mt-4'
+          <input className='montserrat formulario-input text-center text-white mt-4'
             type='number'
             value={persona.edad}
             onChange={(event) => setPersona((anterior) => ({ ...anterior, edad: event.target.value }))}
@@ -38,15 +45,16 @@ const App = () => {
             placeholder='Ingresá tu edad'
             required
           />
-          <div className='text-center p-5'>
-            <button type="submit" className='formulario-btn cursiva btn btn-light'>Enviar</button>
+          <div className='text-center p-4'>
+            <button type="submit" className='formulario-btn cursiva btn text-white'>Enviar</button><br></br>
+            <button type="button" onClick={limpiarFormulario} className='formulario-btnReset montserrat btn'>(Limpiar)</button>
           </div>
         </form>
       </div>
       <div className='overlay'>
         {mensaje && <Mensaje persona={persona} />}
       </div>
-    </>);
+    </>); 
 };
 
 export default App;
